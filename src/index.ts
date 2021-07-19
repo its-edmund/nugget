6aaaed9ea65fa62b3f2b970391f6e3229d5b848e
mxinburritos <mxinburritos@gmail.com>
2021-07-19 02:32:10 -0700

    Add weekly reminder

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
+import cron from "cron";
 import Discord, {
   ClientUser,
   EmbedField,
   EmbedFieldData,
   GuildMemberManager,
   Message,
+  TextChannel,
 } from "discord.js";
 dotenv.config();
 
@@ -12,11 +14,20 @@ const client = new Discord.Client();
 
 let members = [];
 
+client.on("ready", () => {
+  // 50 7 * * 0
+  const scheduledReminder = new cron.CronJob("50 7 * * 0", async () => {
+    const guild = await client.channels.fetch("766184350446125108");
+    (guild as TextChannel).send("@everyone Time for nuggets meeting!");
+  });
+
+  scheduledReminder.start();
+});
+
 client.on("message", async (message) => {
   if (message.content === "&points") {
-    let members = await (
-      await client.guilds.fetch(message.guild.id)
-    ).members.cache;
+    let members = await client.guilds.cache.get("766184350446125106").members
+      .cache;
 
     members = members.filter((member) => {
       console.log(member.user.bot);
